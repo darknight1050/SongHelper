b3e03c7e495fe70fc28b6cdacf443ea15b039521
darknight1050 <darkdeveloping1050@gmail.com>
2020-04-07 15:11:59 +0200

    Android.mk LOCAL_CFLAGS fix

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@ rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
 include $(CLEAR_VARS)
 LOCAL_LDLIBS     := -llog
-LOCAL_CFLAGS     := -DMOD_ID='"SongHelper"' -DVERSION='"0.1.1"' -I"C:/Program Files/Unity/Hub/Editor/2019.3.1f1/Editor/Data/il2cpp/libil2cpp"
+LOCAL_CFLAGS     := -D"MOD_ID=\"SongHelper\"" -D"VERSION=\"0.1.1\"" -I"c:/Program Files/Unity/Hub/Editor/2019.3.1f1/Editor/Data/il2cpp/libil2cpp"
 LOCAL_MODULE     := songhelper
 LOCAL_C_INCLUDES := ./include ./src
 LOCAL_SRC_FILES  := $(call rwildcard,extern/beatsaber-hook/shared/inline-hook/,*.cpp) $(call rwildcard,extern/beatsaber-hook/shared/utils/,*.cpp) $(call rwildcard,extern/beatsaber-hook/shared/inline-hook/,*.c)
